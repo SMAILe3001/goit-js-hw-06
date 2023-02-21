344ab8d380bb0df7a6629f91f320e1702211a861
SMAILe3001 <smaile3001@gmail.com>
2023-02-21 23:26:23 +0200

    exercise 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const ref = {
+	buttomCreateEl: document.querySelector("[data-create]"),
+	buttomDestroyEl: document.querySelector("[data-destroy]"),
+	quantityGenerationEl: document.querySelector("#controls input"),
+	boxesEl: document.querySelector("#boxes"),
+	minBoxSizeEl: 30,
+	step: 10,
+};
+let quantityGeneration;
+
+ref.quantityGenerationEl.addEventListener("input", f => (quantityGeneration = f.currentTarget.value));
+
+ref.buttomCreateEl.addEventListener("click", () => {
+	createBoxes(quantityGeneration);
+});
+
+ref.buttomDestroyEl.addEventListener("click", destroyBoxes);
+
+function createBoxes(i) {
+	let newBoxEl = "";
+	let minBoxSize = ref.minBoxSizeEl;
+
+	for (; i > 0; i -= 1) {
+		newBoxEl += `<div style="width: ${minBoxSize}px; height: ${minBoxSize}px; background-color: ${getRandomHexColor()}"></div>`;
+		minBoxSize += ref.step;
+	}
+
+	ref.boxesEl.innerHTML = newBoxEl;
+}
+
+function destroyBoxes() {
+	ref.boxesEl.innerHTML = "";
 }
